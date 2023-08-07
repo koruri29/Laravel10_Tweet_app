8b1dbbe8026675acedfd55780696b4178bc1c9d2
koruri29 <koruri821@yahoo.co.jp>
2023-08-07 12:59:25 +0900

    fixed web.php

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('tweet.index');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -35,6 +35,8 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::middleware('auth')->group(function() {
+    Route::get('/', [ReadController::class, 'show'])
+        ->name('index');   
     Route::get('/tweet', [ReadController::class, 'show'])
         ->name('index');
     Route::post('/tweet', [CreateController::class, 'create'])
