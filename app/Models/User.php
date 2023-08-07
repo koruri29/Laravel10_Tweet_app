4a09c9be6bee1785c571d54ac4aa02a6f0455474
koruri29 <koruri821@yahoo.co.jp>
2023-08-07 12:19:35 +0900

    fixed User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $guarded = array('id');
+
     /**
      * The attributes that are mass assignable.
      *
@@ -42,4 +44,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function tweets()
+    {
+        return $this->hasMany('App\Models\Tweet');
+    }
 }
