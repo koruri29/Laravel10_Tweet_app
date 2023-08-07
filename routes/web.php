10e78eacd52a210de70b956ec12c83da4deb3633
koruri29 <koruri821@yahoo.co.jp>
2023-08-07 09:33:08 +0900

    認証機能追加

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::middleware('auth')->group(function() {
+    Route::get('/tweet', [ReadController::class, 'show'])
+        ->name('index');
+    Route::post('/tweet', [CreateController::class, 'create'])
+        ->name('create');
+    Route::get('tweet/edit/{tweetId}', [UpdateController::class, 'edit'])
+        ->name('edit.edit');
+    Route::post('tweet/edit/{tweetId}', [UpdateController::class, 'update'])
+        ->name('edit.update');
+    Route::get('tweet/delete/{tweetId}', [DeleteController::class, 'delete'])
+        ->name('delete');
 });
