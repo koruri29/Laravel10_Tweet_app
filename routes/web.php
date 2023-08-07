5fcef9c1b367b99a794df9f4e5283a6744babc59
koruri29 <koruri821@yahoo.co.jp>
2023-08-07 12:56:30 +0900

    fixed web.php

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('tweet.index');
 });
 
 Route::get('/dashboard', function () {
