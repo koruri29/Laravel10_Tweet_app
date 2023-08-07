2a6d73f4fe7cb20c075bf0b4a38ebcd535efb5bd
koruri29 <koruri821@yahoo.co.jp>
2023-08-07 16:52:19 +0900

    slight changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 // import vue from '@vitejs/plugin-vue';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
     plugins: [
@@ -11,6 +12,7 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        basicSsl(),
     ],
     server:{
         host:'0.0.0.0'
